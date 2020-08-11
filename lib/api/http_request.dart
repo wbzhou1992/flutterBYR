63e178367df4943f66c2f2d91e066b8d1129e52b
wbzhou <wbzhou1992@users.noreply.github.com>
2020-08-11 17:37:20 +0800

    Update http_request.dart

--- lib/api/http_request.dart
+++ lib/api/http_request.dart
@@ -21,8 +21,7 @@ class HttpRequest {
       receiveTimeout: 5000,
       headers: {
         //需要配置请求的header可在此处配置
-        'cookie':
-            '_ga=GA1.2.783977894.1565973011; nforum-left=100; left-index=0010000000; nforum[UTMPUSERID]=wbzhou1992; nforum[PASSWORD]=chwkqyuFN0un0OlB6aMDQQ%3D%3D; nforum[UTMPKEY]=76292493; nforum[UTMPNUM]=3672; Hm_lvt_38b0e830a659ea9a05888b924f641842=1576480257,1576548292,1576636840,1576720768; nforum[site]=yamb; Hm_lpvt_38b0e830a659ea9a05888b924f641842=1576720808'
+        
       },
       //请求的Content-Type，默认值是[ContentType.json]. 也可以用ContentType.parse("application/x-www-form-urlencoded")
       //表示期望以那种格式(方式)接受响应数据。接受三种类型 `json`, `stream`, `plain`, `bytes`. 默认值是 `json`,
